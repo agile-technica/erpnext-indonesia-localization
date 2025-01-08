b5a6235b68274ead753faa874bebb8bc75676448
sabiqaz <sabiq.p@agiletechnica.com>
2025-01-08 13:43:20 +0700

    fix/EIL-6179: create doctype Brgjasa ref tree

---
+++ erpnext_indonesia_localization/erpnext_indonesia_localization/doctype/coretax_brgjasa_ref/coretax_brgjasa_ref.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Agile Technica and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class CoreTaxBrgJasaRef(NestedSet):
+	pass
