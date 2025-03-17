dea558e8869a478ebcd81103e909ae1639c319c8
Daniel Sebastian Candra <daniel.c@agiletechnica.co.id>
2025-03-17 14:51:15 +0700

    fix(EIL-7291): remove unused doctype

--- erpnext_indonesia_localization/erpnext_indonesia_localization/doctype/coretax_unitref/coretax_unitref.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Agile Technica and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class CoreTaxUnitRef(Document):
-	pass
