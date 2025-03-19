2354dde8be9a353865b399a28459a04cb1ebfe02
rafly <raflyadie1407@gmail.com>
2025-03-19 11:10:11 +0700

    feat (EIL-7448): create separated function

--- erpnext_indonesia_localization/public/js/customer.js
+++ erpnext_indonesia_localization/public/js/customer.js
@@ -4,19 +4,27 @@ frappe.ui.form.on('Customer', {
     },
 
 	customer_id_type: function (frm) {
-		if (frm.doc.customer_id_type == "TIN") {
-			frm.set_df_property("customer_id_number", "hidden", 1);
-		} else {
-			frm.set_df_property("customer_id_number", "hidden", 0);
-		}
+		set_customer_id_number_visibility(frm);
 	},
 
 	setup: function (frm) {
-		if (frm.doc.coretax_country) {
+		auto_fill_tax_country_code_when_setup(frm);
+	}
+});
+
+function auto_fill_tax_country_code_when_setup(frm) {
+	if (frm.doc.coretax_country) {
 			frappe.db.get_value("Country", frm.doc.coretax_country, "custom_coretax_countryref")
 				.then(r => {
 					frm.set_value("tax_country_code", r.message.custom_coretax_countryref);
 				})
 		}
+}
+
+function set_customer_id_number_visibility(frm) {
+	if (frm.doc.customer_id_type == "TIN") {
+		frm.set_df_property("customer_id_number", "hidden", 1);
+	} else {
+		frm.set_df_property("customer_id_number", "hidden", 0);
 	}
-});
+}
