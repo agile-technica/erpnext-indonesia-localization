7390dc7789e70f3c412c864e619c87ec2d66caad
rafly <raflyadie1407@gmail.com>
2025-03-19 10:36:39 +0700

    feat (EIL-7448): modify customer doctype for coretax enchancments

--- erpnext_indonesia_localization/public/js/customer.js
+++ erpnext_indonesia_localization/public/js/customer.js
@@ -1,5 +1,22 @@
 frappe.ui.form.on('Customer', {
     tax_id: function(frm) {
         frm.set_df_property('company_address_tax_id', 'reqd', frm.doc.tax_id ? 1 : 0);
-    }
+    },
+
+	customer_id_type: function (frm) {
+		if (frm.doc.customer_id_type == "TIN") {
+			frm.set_df_property("customer_id_number", "hidden", 1);
+		} else {
+			frm.set_df_property("customer_id_number", "hidden", 0);
+		}
+	},
+
+	setup: function (frm) {
+		if (frm.doc.coretax_country) {
+			frappe.db.get_value("Country", frm.doc.coretax_country, "custom_coretax_countryref")
+				.then(r => {
+					frm.set_value("tax_country_code", r.message.custom_coretax_countryref);
+				})
+		}
+	}
 });
