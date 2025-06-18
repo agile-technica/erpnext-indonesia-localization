ba08d54ea311d261ea1ad70191607b008d6b85eb
rafly <raflyadie1407@gmail.com>
2025-06-18 13:58:56 +0700

    feat(EIL-8656): adjust function when the template already exists one and adjust unittest

--- erpnext_indonesia_localization/tests/test_utils.py
+++ erpnext_indonesia_localization/tests/test_utils.py
@@ -21,17 +21,24 @@ def test_create_indonesia_localization_tax_template():
 
 	mock_db.exists.side_effect = [False, False]
 
-	mock_doc = MagicMock()
-	mock_get_doc.return_value = mock_doc
-	mock_doc.insert.return_value = None
+	mock_doc_12 = MagicMock()
+	mock_doc_12.name = "PPN Penjualan 12% - PTTC"
+
+	mock_doc_11 = MagicMock()
+	mock_doc_11.name = "PPN Penjualan 11% - PTTC"
+
+	mock_get_doc.side_effect = [mock_doc_12, mock_doc_11]
+	mock_doc_12.insert.return_value = None
+	mock_doc_11.insert.return_value = None
 
 	result = create_indonesia_localization_tax_template(company_name)
 
-	assert result is True
+	assert result == ["PPN Penjualan 12% - PTTC", "PPN Penjualan 11% - PTTC"]
 	assert mock_db.get_value.call_args[0] == ("Company", company_name, "abbr")
 	assert mock_db.exists.call_count == 2
 	assert mock_get_doc.call_count == 2
-	assert mock_doc.insert.call_count == 2
+	assert mock_doc_12.insert.call_count == 1
+	assert mock_doc_11.insert.call_count == 1
 
 
 @patch("erpnext_indonesia_localization.utils.template_tax.frappe")
@@ -54,3 +61,32 @@ def test_create_indonesia_localization_tax_template_when_templates_exist(mock_fr
 	assert mock_frappe.db.exists.call_count == 2
 	assert mock_get_doc.call_count == 0
 	assert mock_get_doc.return_value.insert.call_count == 0
+
+
+@patch("erpnext_indonesia_localization.utils.template_tax.frappe")
+def test_create_indonesia_localization_tax_template_when_one_of_template_missing(mock_frappe):
+	mock_db = mock_frappe.db
+	mock_get_doc = MagicMock()
+	mock_frappe.get_doc = mock_get_doc
+	frappe_mock.utils.now.return_value = "2025-06-12 17:46:06.065257"
+
+	company_name = "PT Test Company"
+	abbr = "PTTC"
+
+	mock_db.get_value.return_value = abbr
+
+	mock_db.exists.side_effect = [False, True]
+
+	mock_doc_12 = MagicMock()
+	mock_doc_12.name = "PPN Penjualan 12% - PTTC"
+
+	mock_get_doc.side_effect = [mock_doc_12]
+	mock_doc_12.insert.return_value = None
+
+	result = create_indonesia_localization_tax_template(company_name)
+
+	assert result == ["PPN Penjualan 12% - PTTC"]
+	assert mock_db.get_value.call_args[0] == ("Company", company_name, "abbr")
+	assert mock_db.exists.call_count == 2
+	assert mock_get_doc.call_count == 1
+	assert mock_doc_12.insert.call_count == 1
